ef0027df31a4efaea16b4748702fead247d256c8
Djailson1 <djailson_silva1@outlook.com>
2022-05-31 22:53:51 -0300

    criação da classe main implementações de objetos com toString, getters e setter

--- AulaPOO/src/Venda.java
+++ AulaPOO/src/Venda.java
@@ -1,3 +1,6 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Venda {
 	
@@ -8,6 +11,63 @@ public class Venda {
 	private String nomeCliente;
 	
 	private String enderecoEntrega;
+	
+	private BigDecimal valorTotal;
+	
+	private List<Produto> listaProdutos = new ArrayList<Produto>();
+	
+	public List<Produto> getListaProdutos() {
+		return listaProdutos;
+	}
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricaoVenda() {
+		return descricaoVenda;
+	}
+
+	public void setDescricaoVenda(String descricaoVenda) {
+		this.descricaoVenda = descricaoVenda;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public String getEnderecoEntrega() {
+		return enderecoEntrega;
+	}
+
+	public void setEnderecoEntrega(String enderecoEntrega) {
+		this.enderecoEntrega = enderecoEntrega;
+	}
+
+	public BigDecimal getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	@Override
+	public String toString() {
+		return "Venda [id=" + id + ", descricaoVenda=" + descricaoVenda + ", nomeCliente=" + nomeCliente
+				+ ", enderecoEntrega=" + enderecoEntrega + ", valorTotal=" + valorTotal + ", listaProdutos="
+				+ listaProdutos + "]";
+	}
+	
 	
 	
 
